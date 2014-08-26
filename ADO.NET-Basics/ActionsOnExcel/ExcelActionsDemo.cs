00b3510d2f126552d3461d6ae00b9c1685a2472a
Dimitar Mitev <dnmitev@gmail.com>
2014-08-27 01:36:33 +0300

    excel tasks are done

---
+++ ADO.NET-Basics/ActionsOnExcel/ExcelActionsDemo.cs
@@ -0,0 +1,47 @@
+﻿namespace ActionsOnExcel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Data;
+    using System.Data.OleDb;
+    using System.Data.SqlClient;
+
+    class ExcelActionsDemo
+    {
+        static void Main()
+        {
+            var connection = new OleDbConnection(ActionsOnExcel.Settings.Default.ConnectionString);
+            connection.Open();
+            using (connection)
+            {
+
+                // 06. Create an Excel file with 2 columns: name and score. Write a program that reads your MS Excel file 
+                // through the OLE DB data provider and displays the name and score row by row. 
+                var oleDbSelectAllData = new OleDbCommand("SELECT * FROM [Exam$]", connection);
+                var reader = oleDbSelectAllData.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        var name = (string)reader["Name"];
+                        var score = (double)reader["Score"];
+
+                        Console.WriteLine("{0} -> Score: {1}", name, score); ;
+                    }
+                }
+
+                // 07. Implement appending new rows to the Excel file. 
+                var oleDbInsertData = new OleDbCommand("INSERT INTO [Exam$](Name, Score) VALUES (@name, @score)", connection);
+
+                oleDbInsertData.Parameters.AddWithValue("@name", "Dinko");
+                oleDbInsertData.Parameters.AddWithValue("@score", 55);
+
+                oleDbInsertData.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ ADO.NET-Basics/ActionsOnNorthwind/NorthwindDemo.cs
@@ -0,0 +1,107 @@
+﻿namespace ActionsOnNorthwind
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Linq;
+
+    internal class NorthwindDemo
+    {
+        private const int OLE_METAFILEPICT_START_POSITION = 78;
+
+        private static void Main()
+        {
+            var dbCon = new SqlConnection(ActionsOnNorthwind.DBConnectionSettings.Default.ConnectionString);
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                // 01. Write a program that retrieves from the Northwind sample database in 
+                // MS SQL Server the number of rows in the Categories table.
+                var sqlCountOfCategories = new SqlCommand("SELECT COUNT(*) FROM Categories", dbCon);
+                var categoriesTotalCount = (int)sqlCountOfCategories.ExecuteScalar();
+                Console.WriteLine("Total count of the 'Categories' is {0}", categoriesTotalCount);
+                PrintSeparator();
+
+                // 02. Write a program that retrieves the name and description of all categories in the Northwind DB. 
+                var sqlRetrieveCategoriesData = new SqlCommand("SELECT CategoryName, Description FROM Categories", dbCon);
+                var categoriesReader = sqlRetrieveCategoriesData.ExecuteReader();
+
+                using (categoriesReader)
+                {
+                    while (categoriesReader.Read())
+                    {
+                        var name = (string)categoriesReader["CategoryName"];
+                        var description = (string)categoriesReader["Description"];
+                        Console.WriteLine("CategoryName: {0}; Description: {1}", name, description);
+                    }
+
+                    PrintSeparator();
+                }
+
+                // 03. Write a program that retrieves from the Northwind database all product categories and the names of the 
+                // products in each category. Can you do this with a single SQL query (with table join)?
+                string retrieveCategorAndProductString = "SELECT p.ProductName, c.CategoryName FROM Categories c INNER JOIN Products p ON c.CategoryID=p.CategoryID";
+
+                var sqlRetrieveCategoriesAndProductName = new SqlCommand(retrieveCategorAndProductString, dbCon);
+                var prductsAndCategoriesReader = sqlRetrieveCategoriesAndProductName.ExecuteReader();
+
+                using (prductsAndCategoriesReader)
+                {
+                    while (prductsAndCategoriesReader.Read())
+                    {
+                        var productName = (string)prductsAndCategoriesReader["ProductName"];
+                        var categoryName = (string)prductsAndCategoriesReader["CategoryName"];
+                        Console.WriteLine("ProductName: {0}; CategoryName: {1}", productName, categoryName);
+                    }
+
+                    PrintSeparator();
+                }
+
+                // 04. Write a method that adds a new product in the products table in the Northwind database. 
+                // Use a parameterized SQL command.
+                string insertProductString = "INSERT INTO Products(ProductName, Discontinued) VALUES (@productName, @discontinued)";
+                var sqlInsertQuery = new SqlCommand(insertProductString, dbCon);
+                sqlInsertQuery.Parameters.AddWithValue("@productName", "Nahuinik");
+                sqlInsertQuery.Parameters.AddWithValue("@discontinued", true);
+                sqlInsertQuery.ExecuteNonQuery();
+
+                // 05. Write a program that retrieves the images for all categories in the Northwind database and stores 
+                // them as JPG files in the file system. 
+                // solution source: http://tihomir.me/tag/northwind
+                var sqlGetPictures = new SqlCommand("SELECT CategoryName, Picture FROM Categories", dbCon);
+                var picturesReader = sqlGetPictures.ExecuteReader();
+
+                using (picturesReader)
+                {
+                    while (picturesReader.Read())
+                    {
+                        string categoryName = ((string)picturesReader["CategoryName"]);
+                        if (categoryName.Contains('/') == true)
+                        {
+                            categoryName = categoryName.Replace('/', ' ');
+                        }
+                        byte[] pictureBytes = picturesReader["Picture"] as byte[];
+
+                        MemoryStream stream = new MemoryStream(
+                            pictureBytes, OLE_METAFILEPICT_START_POSITION,
+                            pictureBytes.Length - OLE_METAFILEPICT_START_POSITION);
+
+                        Image image = Image.FromStream(stream);
+                        using (image)
+                        {
+                            image.Save(string.Format("{0}.jpg", categoryName), ImageFormat.Jpeg);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void PrintSeparator()
+        {
+            Console.WriteLine(new string('-', 60));
+        }
+    }
+}
