36a1306b955af3021ec5e65fadd64b011feba58d
Dimitar Mitev <dnmitev@gmail.com>
2014-09-05 18:48:38 +0300

    all task are done

---
+++ XML-Parsing/XmlToHtml/Demo.cs
@@ -0,0 +1,18 @@
+﻿// 14. an XSL stylesheet, which transforms the file catalog.xml into HTML document, formatted for viewing in a standard Web-browser.
+
+namespace XmlToHtml
+{
+    using System;
+    using System.Linq;
+    using System.Xml.Xsl;
+    
+    internal class Demo
+    {
+        private static void Main()
+        {
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load("../../../catalog.xsl");
+            xslt.Transform("../../../catalog.xml", "../../../catalog.html");
+        }
+    }
+}
